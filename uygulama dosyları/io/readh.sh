fdde617912630450d94500f657a95c572166c372
ALİ HAYDAR <alihaydarsagir29@gmail.com>
2024-01-08 00:04:19 +0300

    Linux_Workspace

---
+++ uygulama dosyları/io/readh.sh
@@ -0,0 +1,5 @@
+echo -n "adiniz : "
+read name
+echo -n "soyadiniz : "
+read surname
+echo "hosgeldiniz, \$name = $name, \$surname = $surname"
---
+++ uygulama dosyları/read.sh
@@ -0,0 +1,5 @@
+echo -n "adiniz: "
+read name
+echo -n "soyadiniz: "
+read surname
+echo "hos geldiniz, \$name =  $name, \$surname = $surname"
